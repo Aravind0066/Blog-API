c5313055d8f29d3fe62a7116c4c34a5fde419608
Aravind M <heyaravind2006@gmail.com>
2026-08-01 16:25:50 +0530

    initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const connectDB = require("./src/config/db");
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+
+
+const app = express();
+app.use(express.json());
+
+const postRoutes = require("./src/routes/postRouter")
+
+
+
+async function startServer(){
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+
+app.get("/" , (req,res)=>{
+    res.send("This is the Home Page.");
+});
+
+app.use("/post", postRoutes);
+
+startServer();
